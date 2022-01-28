901941c4edede56e6b7f02beae3f8e4fa14abe47
Juzy <dochy.ksti@gmail.com>
2022-01-28 16:32:34 +0900

    ばんばんやっていこう

---
+++ ninja_lang/src/imp/compiler/compile.rs
@@ -0,0 +1,7 @@
+use std::path::Path;
+use crate::NlResult;
+use docchi::core::structs::RootObject;
+
+pub fn compile<P : AsRef<Path>>(src_dir : P, target_dir : P) -> NlResult<RootObject>{
+    unimplemented!()
+}
--- ninja_lang/src/imp/mod.rs
+++ ninja_lang/src/imp/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod translator;
 pub(crate) mod structs;
 pub(crate) mod util;
+pub(crate) mod compiler;
