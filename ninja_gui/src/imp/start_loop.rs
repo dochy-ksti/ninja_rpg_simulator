5d2728b6a72c473cdf03e0cf08b995498dff223a
Juzy <dochy.ksti@gmail.com>
2022-03-11 17:18:09 +0900

    やっと動いたか？

--- ninja_gui/src/imp/start_loop.rs
+++ ninja_gui/src/imp/start_loop.rs
@@ -6,7 +6,7 @@ pub(crate) fn start_loop<P : AsRef<Path>>(font_path : P) {
 
     let mut window: PistonWindow = WindowSettings::new(
         "test_window",
-        [500, 500]
+        [1280, 720]
     )
         .build()
         .unwrap();
@@ -16,10 +16,8 @@ pub(crate) fn start_loop<P : AsRef<Path>>(font_path : P) {
     let mut glyphs = Glyphs::new(font_path, window.create_texture_context(), ts).unwrap();
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, d| {
-
-            // Set a white background
-            clear([1.0, 1.0, 1.0, 1.0], g);
-            text::Text::new_color([0.0, 0.0, 0.0, 1.0], 32).draw(
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            text::Text::new_color([1.0, 1.0, 1.0, 1.0], 16).draw(
                 "Hello World",
                 &mut glyphs,
                 &c.draw_state,
