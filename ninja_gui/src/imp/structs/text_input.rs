bbfea65713ebad7df99750750e1f59a68a984377
Juzy <dochy.ksti@gmail.com>
2022-04-11 17:15:47 +0900

    何度車輪を再発明しているのか・・・テキストをどこで改行するかをやるルーチンをやっている

--- ninja_gui/src/imp/structs/text_input.rs
+++ ninja_gui/src/imp/structs/text_input.rs
@@ -1,5 +1,8 @@
+use crate::GuiID;
+
 #[derive(Debug, Clone)]
 pub struct TextItem {
+    id : GuiID,
     title : String,
     next_indexes : Vec<usize>,
     prev_indexes : Vec<usize>,
@@ -13,8 +16,9 @@ pub struct TextInput {
 
 impl TextItem {
     pub fn new(title : String, next_indexes : Vec<usize>, prev_indexes : Vec<usize>) -> TextItem {
-        TextItem { title, next_indexes, prev_indexes }
+        TextItem { id : GuiID::new(), title, next_indexes, prev_indexes }
     }
+    pub fn id(&self) -> &GuiID{ &self.id }
     pub fn title(&self) -> &str{ &self.title }
     pub fn next_indexes(&self) -> &[usize]{ &self.next_indexes }
     pub fn prev_indexes(&self) -> &[usize]{ &self.prev_indexes }
