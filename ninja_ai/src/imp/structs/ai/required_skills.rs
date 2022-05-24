ae06672de01471f757d6839dec589e528260d260
juzu <dochy.ksti@gmail.com>
2022-05-24 10:51:52 +0900

    IDはu32で扱うことにする。stringのIDを連番とかのu32に変換する処理が必要だが、そのくらいは外部に任せるべきか

--- ninja_ai/src/imp/structs/ai/required_skills.rs
+++ ninja_ai/src/imp/structs/ai/required_skills.rs
@@ -1,8 +1,16 @@
+use crate::imp::structs::skill_id::SkillID;
+
 pub(crate) struct RequiredSkills{
     map : Vec<u32>
 }
 
 impl RequiredSkills{
     pub(crate) fn new(skill_len : usize) -> RequiredSkills{ RequiredSkills{ map : Vec::with_capacity(skill_len) } }
+    pub(crate) fn overwrite(&mut self, r : &RequiredSkills){
+        self.map.copy_from_slice(&r.map);
+    }
+    pub(crate) fn set(&mut self, id : SkillID, val : u32){
+        self.map.get_mut()
+    }
 }
 
--- ninja_ai/src/imp/structs/skill_id.rs
+++ ninja_ai/src/imp/structs/skill_id.rs
@@ -4,4 +4,5 @@ pub struct SkillID(u32);
 impl SkillID{
     pub fn new(n : u32) -> SkillID{ SkillID(n) }
     pub fn num(&self) -> u32{ self.0 }
+    pub fn len() -> usize{ 8? }
 }
