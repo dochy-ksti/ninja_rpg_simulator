1776eebc8f9441b8d62348c1c29e99c4cb5dfa3b
Juzy <dochy.ksti@gmail.com>
2022-05-31 18:03:21 +0900

    あまりにもややこしいから単純化していこう。どうせ単純化しないとDPらしきものも走らない

--- ninja_ai/src/imp/structs/ai/skillmap/available_trainings.rs
+++ ninja_ai/src/imp/structs/ai/skillmap/available_trainings.rs
@@ -5,17 +5,30 @@ use crate::imp::structs::ai::skillmap::training::Training;
 
 pub(crate) struct AvailableTrainings{
     bh : BinaryHeap<AvailableTraining>,
+    current_repeatable : Option<AvailableTraining>,
 }
 
 impl AvailableTrainings{
     pub(crate) fn new() -> AvailableTrainings{
-
+        AvailableTrainings{ bh : BinaryHeap::new(), current_repeatable : None }
+    }
+    pub(crate) fn push(&mut self, at : AvailableTraining){
+        self.bh.push(at)
+    }
+    pub(crate) fn current_repeatable(&mut self) -> Option<&AvailableTraining>{
+        self.current_repeatable.as_ref()
+    }
+    pub(crate) fn set_current_repeatable(&mut self, at : AvailableTraining){
+        self.current_repeatable = Some(at);
     }
     pub(crate) fn peek(&self) -> Option<&AvailableTraining>{
         self.bh.peek()
     }
     pub(crate) fn pop(&mut self) -> Option<AvailableTraining>{
         self.bh.pop()
+    }
+    pub(crate) fn clear_heap(&mut self){
+        self.bh.clear()
     }
 }
 
@@ -41,7 +54,7 @@ impl PartialOrd<Self> for AvailableTraining {
 
 impl Ord for AvailableTraining{
     fn cmp(&self, other: &Self) -> Ordering {
-        other.slope.cmp(&self.slope)
+        self.slope.cmp(&other.slope)
     }
 }
 
