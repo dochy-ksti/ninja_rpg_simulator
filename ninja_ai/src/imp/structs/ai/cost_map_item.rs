06122c92a3b8e71943578f863cd670dfb8dd2bf6
Juzy <dochy.ksti@gmail.com>
2022-06-23 17:38:15 +0900

    警告が多すぎるから減らそうと思ったけどむりやった

--- ninja_ai/src/imp/structs/ai/cost_map_item.rs
+++ ninja_ai/src/imp/structs/ai/cost_map_item.rs
@@ -6,6 +6,7 @@ pub(crate) struct CostMapItem{
 
 pub(crate) struct CostItem{
     distance : u32,
+    iteration : u32,
     skills : RequiredSkills,
 }
 
@@ -13,14 +14,20 @@ impl CostMapItem{
     pub(crate) fn no_data() -> CostMapItem{ CostMapItem{ item : None } }
     pub(crate) fn reached() -> CostMapItem{ CostMapItem{ item : Some(CostItem::empty()) } }
     pub(crate) fn reachable(&self) -> bool{ self.item.is_some() }
+    pub(crate) fn item(&self) -> Option<&CostItem>{ self.item.as_ref() }
 }
 
 impl CostItem{
-    pub(crate) fn new(distance : u32) -> CostItem{
-        CostItem{ distance, skills : RequiredSkills::new() }
+    pub(crate) fn new(distance : u32, iteration : u32) -> CostItem{
+        CostItem{ distance, iteration, skills : RequiredSkills::new() }
     }
 
     pub(crate) fn empty() -> CostItem{
-        CostItem{ distance : 0, skills : RequiredSkills::empty() }
+        CostItem{ distance : 0, iteration : 0, skills : RequiredSkills::empty() }
     }
+
+    pub(crate) fn distance(&self) -> u32{ self.distance }
+    pub(crate) fn iteration(&self) -> u32{ self.iteration }
+
+    pub(crate) fn is_reached(&self) -> bool{ self.skills.is_empty() }
 }
--- ninja_ai/src/imp/think/visit.rs
+++ ninja_ai/src/imp/think/visit.rs
@@ -1,9 +1,34 @@
 use crate::imp::structs::ai::cost_map::CostMap;
+use crate::imp::structs::ai::cost_map_item::CostMapItem;
+use crate::imp::structs::ai::skillmap::skill_map::SkillMap;
 use crate::imp::structs::event::Event;
+use crate::imp::structs::event_id::EventID;
 use crate::imp::structs::events::Events;
 
-pub(crate) fn visit(ev : &Event, evs : &Events, cost_map : &mut CostMap){
+pub(crate) fn visit(ev : &Event, evs : &Events, cost_map : &mut CostMap, skill_map : &SkillMap, iteration : u32){
+    if
     for w in ev.walls(){
+        for wall in w.walls(){
+            if let Some(cond) = wall.cond(){
+                cond.id()
+            }
+        }
+    }
+}
 
+fn should_visit(iteration : u32, item : &CostMapItem) -> bool{
+    if let Some(item) = item.item(){
+        if item.is_reached(){
+            //到達済み
+            false
+        } else if item.iteration() < iteration{
+            //過去のiterationで計算したが今回はまだ計算していない
+            true
+        } else{
+            false
+        }
+    } else{
+        //まだ到達していない
+        true
     }
 }
